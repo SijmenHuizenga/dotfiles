d5cc8049b06305d65833188ff43fd174d6f5a770
sijmenhuizenga <sijmenhuizenga@gmail.com>
2019-02-10 12:56:53 +0100

    updates

--- bin/backup.sh
+++ bin/backup.sh
@@ -1,10 +1,24 @@
-rsync -avhP --delete --exclude '$RECYCLE.BIN' --exclude 'pagefile.sys' --exclude 'System Volume Information' --exclude '.Trash-1000' --exclude 'Games' /media/Archief /run/media/sijmen/Sijmen\ Blue/
+set -e
+
+target=/media/sdc1
+
 rsync -avhP --delete \
-   --exclude '$RECYCLE.BIN' \
-   --exclude '.Trash-1000' \
+  --exclude '$RECYCLE.BIN' \
+  --exclude 'pagefile.sys' \
+  --exclude 'System Volume Information' \
+  --exclude '.Trash-1000' \
+  --exclude 'Games' \
+ /media/archief/ $target/Archief
+
+rsync -avhP --delete \
    --exclude node_modules/ \
    --exclude vendor/ \
    --exclude build/ \
    --exclude bower_components/ \
- /media/Data /run/media/sijmen/Sijmen\ Blue/
-rsync -avhP --delete ~/.ssh /run/media/sijmen/Sijmen\ Blue/
+ ~/projects/ $target/Data/projects
+
+rsync -avhP --delete ~/.ssh/ $target/.ssh
+
+rsync -avhP --delete ~/audio/ $target/Data/audio
+
+rsync -avhP --delete ~/documents/ $target/Data/documents
